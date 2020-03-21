0a51a6b7866173a55aa144cacd2b1ff1c4311ff3
Zakaria Makrelouf <zakaria.makrelouf@tajawal.com>
2020-03-21 15:50:32 +0400

    Required libraries.

--- setup.py
+++ setup.py
@@ -13,10 +13,20 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/kamranahmedse/itomate",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "iterm2>=1.1",
+        "PyYAML>=5.3.1",
+
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: MacOS",
     ],
-    python_requires='>=3.0',
+    python_requires='>=3.7.0',
+    license="MIT",
+    entry_points="""
+        [console_scripts]
+        itomate=itomate:main
+    """
 )
