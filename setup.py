aa81908886c25fc45f12a722e573484622e9f80f
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-03-21 16:00:38 +0400

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="itomate",
-    version="0.0.1",
+    version="0.2.5",
     author="Kamran Ahmed",
     author_email="kamranahmed.se@gmail.com",
     description="Automate your iTerm layouts and workflows",
