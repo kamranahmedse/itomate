88aaf39abb1b66c2ed3d74b417cde91be8ff0e00
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-03-21 16:49:43 +0400

    Bump version 0.2.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="itomate",
-    version="0.2.8",
+    version="0.2.9",
     author="Kamran Ahmed",
     author_email="kamranahmed.se@gmail.com",
     description="Automate your iTerm layouts and workflows",
